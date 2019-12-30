956a0fc075dee22afd6320e45c5f185d31922f3c
Vitali Mueller <vmueller@ltg.com>
2019-12-30 20:28:52 +0000

    created static files and templates

--- app.py
+++ app.py
@@ -1,12 +1,43 @@
-from flask import Flask
+from flask import (
+    Flask,
+    render_template,
+    url_for,
+)
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Vitali Mueller',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'December 30, 2019',
+    },
+    {
+        'author': 'Vitali Mueller',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'December 31 2019',
+    },
+    {
+        'author': 'Vitali Mueller',
+        'title': 'Blog Post 3',
+        'content': 'Third post content',
+        'date_posted': 'Jan 1, 2020',
+    }
+
+]
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return render_template('home.html', posts=posts, title='Home Page')
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About Page')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
